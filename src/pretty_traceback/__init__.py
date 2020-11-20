67352e18cd7f9780128106d86eaa778fdffb953a
Manuel Barkhau <mbarkhau@gmail.com>
2020-11-20 01:13:00 +0000

    bump version to 2020.1015

--- src/pretty_traceback/__init__.py
+++ src/pretty_traceback/__init__.py
@@ -9,7 +9,7 @@ from .hook import uninstall
 from .formatting import LoggingFormatter
 from .formatting import LoggingFormaterMixin
 
-__version__ = "2020.1014"
+__version__ = "2020.1015"
 
 
 __all__ = [
